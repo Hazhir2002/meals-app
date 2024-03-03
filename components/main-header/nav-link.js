6f3ed079b14f3f79924a5da112b5f3282da5ac3e
Hazhir2002 <HazhirYousefi2002@gmail.com>
2024-03-03 10:52:16 +0330

    Finished the project

---
+++ components/main-header/nav-link.js
@@ -0,0 +1,20 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import classes from "./nav-link.module.css";
+import Link from "next/link";
+
+export default function NavLink({ href, children }) {
+  const path = usePathname();
+
+  return (
+    <Link
+      href={href}
+      className={`${classes.link} ${
+        path.startsWith(href) ? classes.active : undefined
+      }`}
+    >
+      {children}
+    </Link>
+  );
+}
